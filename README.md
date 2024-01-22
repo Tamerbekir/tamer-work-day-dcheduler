e6e7db761a34b41696219e431331b24130811a18
Tamerbekir <astoryofame@gmail.com>
2024-01-22 14:43:28 -0500

    fixed daily planner that now shows the correct colors for the correct times. Added for loop for storage. Updated README file

--- README.md
+++ README.md
@@ -1,40 +1,36 @@
 # Tamer Work Day Schedueler
 
-# 05 Third-Party APIs: Work Day Scheduler
-
-## Your Task
-
-Create a simple calendar application that allows a user to save events for each hour of a typical working day (9am&ndash;5pm) by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
-
 ## User Story
 
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+AS AN employee with a busy schedule <br>
+I WANT to add important events to a daily planner<br>
+SO THAT I can manage my time effectively<br>
 
 ## Acceptance Criteria
 
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+GIVEN I am using a daily planner to create a schedule<br>
+WHEN I open the planner<br>
+THEN the current day is displayed at the top of the calendar<br>
+WHEN I scroll down<br>
+THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm<br>
+WHEN I view the timeblocks for that day<br>
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future<br>
+WHEN I click into a timeblock<br>
+THEN I can enter an event<br>
+WHEN I click the save button for that timeblock<br>
+THEN the text for that event is saved in local storage<br>
+WHEN I refresh the page<br>
+THEN the saved events persist<br>
 
 The following animation demonstrates the application functionality:
 
-<!-- @TODO: create ticket to review/update image) -->
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+
+## Description
+
+Here is a daily player where a user can set up their daily to-do's, which will show the user the past (grey), the current time (red) and the future open availability (green). When the user enters their current or future plans, the schedule will save to their local machine.
+
+## Screenshot
+
+
+## Link
+<a href="https://tamerbekir.github.io/tamer-mets-quiz/">Link to Daily Scheduler</a>
--- assets/script.js
+++ assets/script.js
@@ -5,79 +5,55 @@
 const timeDisplay = $('#currentDay');
 const saveAppt = $(".saveBtn")
 
-let currentHour = dayjs().hour()
+// let currentHour = dayjs().hour()
 let timeBlock = $(".time-block")
-
-
+let twentyFourHour = dayjs().format("H");
 
 function displayTime() {
-  let rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  let rightNow = dayjs().format('MMM DD, YYYY');
   timeDisplay.text(rightNow);
-}
+} 
 
-function saveReminder() {
-alert("Your schedule has been saved")
-}
 
-// GETTING AND SETTING SCHEDULE TO STORAGE
+//! GETTING AND SETTING SCHEDULE TO STORAGE
 $(saveAppt).on('click', function () {
   // THIS = saveAppt function
   let descriptionValue = $(this).siblings('.scheduleText').val();
   let hourValue = $(this).parent().attr('id');
   localStorage.setItem(hourValue, descriptionValue);
   console.log(hourValue, descriptionValue)
-  saveReminder()
 });
 
-//for Loop saves all entry to local storage
+//for Loop saves all entry to local storage, 12 being the highest number
 for (let i = 0; i <= 12; i++) {
   $(`#hour${i} .scheduleText`).val(localStorage.getItem(`hour${i}`));
 }
 
 
-//! PAST, FUTURE AND PRESENT ATTRIBUTES TO SCHEDULE
-//! create variable for  time
-//! if statements for past, present and future
-
+//! PAST, FUTURE AND PRESENT ATTRIBUTES TO SCHEDULE. NOT WORKING AS INTENDED
 function scheduleAttributes() {
-  $.each(timeBlock, function() {
-    let scheduledTime = parseInt($(this).attr('id').split("hour")[1])
-    // if schedule is BEFORE the current hour
-    if (scheduledTime < currentHour) {
-      $(this).addClass("past")
-    } else if (scheduledTime === currentHour) {
-      // if schedule is WITHIN the current hour
-      $(this).addClass("present")
+  let twentyFourHour = dayjs().format("H");
+
+  $(timeBlock).each(function () {
+    let scheduledPlanner = parseInt($(this).attr("id").split("hour")[1]);
+    console.log(scheduledPlanner, twentyFourHour);
+
+    // Apply classes when comapring of scheduledPlanner and twentyFourHour
+    if (scheduledPlanner < twentyFourHour) {
+      // If before current time
+      $(this).addClass("past").removeClass("future present");
+    } else if (scheduledPlanner === twentyFourHour) {
+      // If within current time
+      $(this).addClass("present").removeClass("past future");
     } else {
-      // if schedule made AFTER the current hour
-      $(this).addClass("future")
+      // Otherwise, all time is in the future
+      $(this).addClass("future").removeClass("past present");
     }
   });
 }
-
-
 
 scheduleAttributes()
 displayTime()
 setInterval(displayTime, 1000);
 
-
-// TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. 
-
-// HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
-// TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour.
-
-// HINTS: How can the id attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-
-// TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. ✅
-
-
-// HINT: How can the id attribute of each time-block be used to do this?
-
-// TODO: Add code to display the current date in the header of the page ✅
 
